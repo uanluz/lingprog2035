ab7342e3c0bfeb2d7f0617fb44001c2a0d21950b
uanluz <uandesonifmt2024@gmail.com>
2024-10-05 00:26:08 +0000

    fiz nada

--- ola.c
+++ ola.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 void main () {
-    printf("ola mundo!");
+    int x = 0; // crindo uma variavel inteira
+    printf("valor de x : %d\n",x);
+    x++ ; // incrementando x em 1 unidade
+    printf("valor de x: %d\n",x);
+    float y = 12.345678; // crindo uma variavel real
+    printf ("valor de y : %f\n,y");
+    //imprimindo o valor de y com 2 casas
+    printf("valor de y: %.2f\n",y);
+    //1 caractere deve estar entre apostrofo
+    char sexo = "m"; // variavel que aceita 1 caractere
+    printf("sexo: %c\n",sexo);
+    //criando uma variavel string (texto)
+    char nome [30] = "joao";
+    printf("nome; %s\n",nome);
+    //imprimindo a primeira letra do mome
+    printf ("primeira letra : %c\n", nome[0]);
+    printf (" ola mundo!");
 }
